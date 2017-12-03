5d292a5667c6b17171262e17f5deee8e8af75bb1
Christoph <christoph.tittel@gmail.com>
2017-12-03 23:42:45 +0100

    Org Export hinzugefuegt. Klassen: koma-article, book

--- init.el
+++ init.el
@@ -1,11 +1,11 @@
-;============ Meine emacs Init ==================
-;------------ Benötigte Packages
-;-------------------------------
-;- org-ref
+					;============ Meine emacs Init ==================
+					;------------ Benötigte Packages
+					;-------------------------------
+					;- org-ref
 
 
 
-;---------------------- Paktearchive
+					;---------------------- Paktearchive
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
@@ -14,7 +14,7 @@
 (package-initialize)
 
 
-;------------------------------ keine Ahnung
+					;------------------------------ keine Ahnung
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -29,46 +29,36 @@
  )
 
 
-;--------------------------- Lade nicht vorhandene packages
+					;--------------------------- Lade nicht vorhandene packages
 (unless package-archive-contents
   (package-refresh-contents))
 (package-install-selected-packages)
 
 
 
-;----------------------------- Backup Dateien
+					;----------------------------- Backup Dateien
 (setq
-backup-by-copying t      ; don't clobber symlinks
-delete-old-versions t
-kept-new-versions 6
-kept-old-versions 2
-version-control t       ; use versioned backups
-)
+ backup-by-copying t      ; don't clobber symlinks
+ delete-old-versions t
+ kept-new-versions 6
+ kept-old-versions 2
+ version-control t       ; use versioned backups
+ )
 
-;------------------------ Startup Screen
+					;------------------------ Startup Screen
 (setq inhibit-startup-screen t)
 
-
-;--------------------------------- org-agenda
+					;============================================ ORG-MODE
+					;--------------------------------- org-agenda
 (setq org-agenda-files '("C:/Users/User/Google Drive/Notes/"))
 
-
-
-
-;-------------------------------- org babel (hier python)
+					;-------------------------------- org babel (hier python)
 (org-babel-do-load-languages
-'org-babel-load-languages
-'((emacs-lisp . nil)
-(python . t)))
+ 'org-babel-load-languages
+ '((emacs-lisp . nil)
+   (python . t)))
 
-
-
-
-
-
-
-
-;--------------------------- Tastenkombinationen
+					;--------------------------- Tastenkombinationen
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
@@ -76,3 +66,22 @@ version-control t       ; use versioned backups
 
 (require 'org-ref)
 (global-set-key (kbd "C-c l") 'org-store-link)
+
+					;------------------------------Latex Export Klassen
+(with-eval-after-load "ox-latex"
+  (add-to-list 'org-latex-classes
+               '("koma-article" "\\documentclass{scrartcl}"
+                 ("\\section{%s}" . "\\section*{%s}")
+                 ("\\subsection{%s}" . "\\subsection*{%s}")
+                 ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+                 ("\\paragraph{%s}" . "\\paragraph*{%s}")
+                 ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+
+  '("book" "\\documentclass{book}"
+    ("\\part{%s}" . "\\part*{%s}")
+    ("\\chapter{%s}" . "\\chapter*{%s}")
+    ("\\section{%s}" . "\\section*{%s}")
+    ("\\subsection{%s}" . "\\subsection*{%s}")
+    ("\\subsubsection{%s}" . "\\subsubsection*{%s}"))
+  )
+
