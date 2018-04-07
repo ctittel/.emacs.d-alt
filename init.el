de32bec27b7f475d9c93b14c2082d63b26d96e62
Christoph <christoph.tittel@gmail.com>
2018-04-07 13:28:55 +0200

    -- chris-create-all-backlinks created and works

--- init.el
+++ init.el
@@ -81,7 +81,7 @@ static char *gnus-pointer[] = {
  '(org-agenda-files (quote nil))
  '(package-selected-packages
    (quote
-    (markdown-mode org csv-mode alect-themes zenburn-theme w3 deft elpy plantuml-mode slime helm-bibtex langtool flycheck company helm org-ref)))
+    (spacemacs-theme markdown-mode org csv-mode alect-themes zenburn-theme w3 deft elpy plantuml-mode slime helm-bibtex langtool flycheck company helm org-ref)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(tool-bar-mode nil)
  '(vc-annotate-background "#2B2B2B")
